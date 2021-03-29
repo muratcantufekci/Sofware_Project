76a94c003b19c01a5367859acd6bff2eb2867420
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-03-29 21:57:58 +0300

    login

---
+++ SoftwareProject/Controllers/SecurityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareProject.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
